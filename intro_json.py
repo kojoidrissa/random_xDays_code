9973b382c95ba60a121db558fc587cfabb369b84
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-10-09 13:08:57 -0500

    added view counts to code; formatted count number

--- intro_json.py
+++ intro_json.py
@@ -1,20 +1,21 @@
-print(str.upper("Nine lines of code with stock Python 3"))
-import json
-from urllib.request import urlopen
-url = "https://gdata.youtube.com/feeds/api/standardfeeds/top_rated?alt=json" 
-response = urlopen(url)
-contents = response.read()
-text = contents.decode('utf8')
-data = json.loads(text)
-for video in data['feed']['entry'][0:5]:
-    print(video['title']['$t'])
+# print(str.upper("Nine lines of code with stock Python 3"))
+# import json
+# from urllib.request import urlopen
+# url = "https://gdata.youtube.com/feeds/api/standardfeeds/top_rated?alt=json" 
+# response = urlopen(url)
+# contents = response.read()
+# text = contents.decode('utf8')
+# data = json.loads(text)
+# for video in data['feed']['entry'][0:5]:
+#     print(video['title']['$t'])
 
 print()
-print("Six lines of code with additional 'requests' library")
+print(str.upper("Six lines of code with additional 'requests' library"))
 #shorter version, using request external library
 import requests
 url = "https://gdata.youtube.com/feeds/api/standardfeeds/top_rated?alt=json" 
 response = requests.get(url)
 data = response.json()
 for video in data['feed']['entry'][0:6]:
-    print(video['title']['$t'])
+    view_count = int(video['yt$statistics']['viewCount']) #converts viewCount into an int
+    print(video['title']['$t'], "with", '{:,}'.format(view_count), "views") #second 1/2 formats the int with thousands separators
