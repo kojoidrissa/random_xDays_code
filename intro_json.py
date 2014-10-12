48a2e9422345c58c15fb47c2a48659f020494030
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-10-11 20:48:34 -0500

    added code to show me the various catagories in the YouTube feed data.

--- intro_json.py
+++ intro_json.py
@@ -16,6 +16,11 @@ import requests
 url = "https://gdata.youtube.com/feeds/api/standardfeeds/top_rated?alt=json" 
 response = requests.get(url)
 data = response.json()
-for video in data['feed']['entry'][0:6]:
+for video in data['feed']['entry'][0:5]:
     view_count = int(video['yt$statistics']['viewCount']) #converts viewCount into an int
     print(video['title']['$t'], "with", '{:,}'.format(view_count), "views") #second 1/2 formats the int with thousands separators
+
+#this shows me the various data catagories stored in each YouTube Entry listing
+for i in data['feed']['entry']['title']:
+    print (i)
+#Looks like I' after ['title']['$t'] and ['yt$statistics']
