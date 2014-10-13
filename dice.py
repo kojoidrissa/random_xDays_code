9f5865208eefa24a3c43d4c71a6e196ecb05b48d
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-10-13 13:05:06 -0500

    Left comments for my next step in development

--- dice.py
+++ dice.py
@@ -2,6 +2,11 @@
 
 import random as rnd
 
+#Update this to include a loop that lets you keep rolling and an option to end (break)
+#when the input is "End". I should only convert the input to an int
+#AFTER I've determined that it's not "End". Eventually I'll want to test for
+#non-valid entries (non-numbers), but I'm not sure how to do that yet. :-)
+
 s = int(input("What die would you like to roll? Enter the number of sides "))
 
 roll = rnd.randint(1, s)
