f5d2cefd3181af23de509d5d8060e577607a16fb
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-10-12 22:35:30 -0500

    finished fizzbuzz_while.py

--- fizzbuzz_while.py
+++ fizzbuzz_while.py
@@ -1,8 +1,15 @@
 #attempting the Fizzbuzz problem with a While loop
 
 i = 1
-while i <= 10:
-    print i
+while i <= 100:
+    if i % (3*5) == 0:
+        print ("FizzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print i
     i +=1
 
 
