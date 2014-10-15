06abf03cc0ae3fc6e2dc03668cdd9fb4bf7c43c1
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-10-14 20:33:33 -0500

    started on loneSum.py

---
+++ loneSum.py
@@ -0,0 +1,16 @@
+#Copied this from XLindawang http://30daysofgithub.tumblr.com/post/100039983255/day-8
+#http://codingbat.com/prob/p148972
+
+def loneSum(ints)
+
+'''
+iterable of ints --> int 
+
+Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum. 
+
+loneSum(1, 2, 3) → 6
+loneSum(3, 2, 3) → 2
+loneSum(3, 3, 3) → 0
+'''
+    
+
