ee919c5b11657a58f7eeb7aac2e80cdbe9500e14
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-10-17 13:10:22 -0500

    fixed an error in the "NEW CODE" section; added some notes there.

--- kpop_stats.py
+++ kpop_stats.py
@@ -20,7 +20,11 @@ for video in data['feed']['entry'][0:5]:
     view_count = int(video['yt$statistics']['viewCount']) #converts viewCount into an int
     print(video['title']['$t'], "with", '{:,}'.format(view_count), "views") #second 1/2 formats the int with thousands separators
 
+print()
+print("NEW CODE STARTS HERE!!!")
+print('''This is showing me the results from the "data = response.json"; the type and length of each
+    element in "data['feed']['entry']" ''')
 #this shows me the various data catagories stored in each YouTube Entry listing
-for i in data['feed']['entry']['title']:
-    print (i)
-#Looks like I' after ['title']['$t'] and ['yt$statistics']
+for i in data['feed']['entry']:
+    print (type(i), len(i))
+#Looks like I'm after ['title']['$t'] and ['yt$statistics']
