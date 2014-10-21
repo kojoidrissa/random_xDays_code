95f6d3023369b33eee8d1aaa2df25416cb77bde0
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-10-21 12:35:37 -0500

    wrote my first class! :-)

--- casino.py
+++ casino.py
@@ -1,1 +1,11 @@
 #file for "Classes" section of Introduction to Python
+class Greeter(object):
+    def hello(self):
+        print("Hello")
+
+    def goodbye(self):
+        print("Holla!!")
+
+g = Greeter()
+g.hello()
+g.goodbye()
