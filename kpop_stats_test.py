2ede55c820ab352631ad98eb119e318dd5945fa2
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-11-19 19:42:53 -0600

    added new testing file for kpop_stats (not unit testing, testing new ideas); it'll need it's own directory soon; also got rid of the profiler data.

---
+++ kpop_stats_test.py
@@ -0,0 +1,18 @@
+import requests
+url = "https://gdata.youtube.com/feeds/api/standardfeeds/top_rated?alt=json" 
+response = requests.get(url)
+data = response.json()
+for video in data['feed']['entry'][0:5]:
+    view_count = int(video['yt$statistics']['viewCount']) #converts viewCount into an int
+    print(video['title']['$t'], "with", '{:,}'.format(view_count), "views") #second 1/2 formats the int with thousands separators
+
+print()
+print("NEW CODE STARTS HERE!!!")
+print('''This is showing me the results from the "data = response.json"; the type and length of each
+    element in "data['feed']['entry']" ''')
+#this shows me the various data catagories stored in each YouTube Entry listing
+for i in data['feed']['entry']:
+    print (type(i), len(i))
+    # print (i['yt$statistics']['viewCount'])
+    print (i['title']['$t'])
+#Looks like I'm after ['title']['$t'] and ['yt$statistics']
