66094a076d6b20f4843f1c2511d39b55d2113403
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-12-02 19:55:29 -0600

    added notes on new method for testing my anonymize function

--- anonymize.py
+++ anonymize.py
@@ -9,9 +9,18 @@ import hashlib
 # print(len(hex_dig))
 # print(\n)
 # print("New EXAMPLE")
+"""
+New approach to testing this idea
+1. create a sequence of numbers, approximateing the number of names/records
+2. hash each number
+3. make list of  first 5 char of each hash; search it for duplicates
+4. repeat step 4 with 1st and 2nd 5 char of each hash (simulate personal, family name)
+"""
 names = ["John", "Jon", "Jonny", "Juan", "Johan"]
 
 for n in names:
     hash_object = hashlib.sha1(bytes(n, encoding='utf-8'))
     hex_dig = hash_object.hexdigest()
     print (hex_dig, "; ", len(hex_dig))
+
+
