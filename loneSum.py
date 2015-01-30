458573b940fb64d1219adbeb7298c54221708481
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-01-30 17:37:37 -0600

    improved docstring formatting

--- loneSum.py
+++ loneSum.py
@@ -6,7 +6,8 @@ def loneSum(a, b, c):
     '''
     int, int, int --> int 
 
-    Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum. 
+    Given 3 int values, a b c, return their sum. However, if one of the values is 
+    the same as another of the values, it does not count towards the sum. 
 
     loneSum(1, 2, 3) → 6
     loneSum(3, 2, 3) → 2
