8983e0a7f1184ae209d5df5f794786c19dcde21f
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-10-19 13:25:27 -0500

    added ascii_range.py to repo

---
+++ ascii_range.py
@@ -0,0 +1,5 @@
+#For generating a range of ascii characters. I should look into the 
+#unicode way to do this too
+#I came up with this while working on the Umbelmania challenge
+
+moveset = [chr(num+65) for num in range(11)]
